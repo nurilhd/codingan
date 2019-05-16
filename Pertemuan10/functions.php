dc151eb2bebc2195f0b1c67097aeafec58b74d98
nurilhd <43733888+nurilhd@users.noreply.github.com>
2019-05-16 23:40:49 +0700

    php CRUD
    01

---
+++ Pertemuan10/functions.php
@@ -0,0 +1,36 @@
+ <?php
+ $conn = mysqli_connect("localhost", "root","","phpdasar");
+
+function query($query){
+  global $conn;
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ( $row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+
+function tambah($data){
+   global $conn;
+  //ambil data dr setiap element form
+  $nama = htmlspecialchars($data["nama"]);
+  $nim = htmlspecialchars($data["nim"]);
+  $email = htmlspecialchars($data["email"]);
+  $jurusan = htmlspecialchars($data["jurusan"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+
+  //query insert data
+  $query = "INSERT INTO mahasiswa VALUES('','$nama','$nim','$email','$jurusan','$gambar')";
+  mysqli_query($conn, $query);
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id){
+  global $conn;
+  mysqli_query($coon, "DELETE FROM mahasiswa WHERE id = $id");
+  return mysqli_affected_rows($conn);
+}
+
+ ?>
---
+++ Pertemuan10/index.php
@@ -0,0 +1,53 @@
+<?php
+
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa")
+ ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Halaman Admin</title>
+  </head>
+  <body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+
+      <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Nama</th>
+        <th>Nim</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+        <th>Gambar</th>
+      </tr>
+
+<?php foreach ($mahasiswa as $row):
+?>
+
+      <tr>
+        <td><?= $row["id"]; ?></td>
+        <td>
+            <a href="">ubah</a>
+            <a href="hapus.php?id=<?= $row["id"]; ?>">hapus</a>
+        </td>
+        <td><?= $row["nama"]; ?></td>
+        <td><?= $row["nim"]; ?></td>
+        <td><?= $row["email"]; ?></td>
+        <td><?= $row["jurusan"]; ?></td>
+        <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+      </tr>
+
+
+<?php endforeach;
+ ?>
+
+  </body>
+</html>
