80ee2a60c0d58028e3c32dd25cbf895be3943909
nurilhd <43733888+nurilhd@users.noreply.github.com>
2019-05-20 18:26:18 +0700

    Registrasi

---
+++ Pertemuan14/ubah.php
@@ -0,0 +1,74 @@
+<?php
+require 'functions.php';
+
+//ambil data di urldecode
+$id = $_GET["id"];
+// query data mahasiswa bedasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tombol sumbit sudah ditekan atau belum
+if (isset($_POST["submit"])){
+  //cek apakah data berhasil atau tidak
+  if(ubah($_POST)>0){
+    echo "
+      <script>
+          alert('data berhasil diubah');
+          document.location.href='index.php';
+      </script>
+    ";
+}else{
+  echo "
+    <script>
+        alert('data gagal diubah');
+        document.location.href='index.php';
+    </script>
+  ";
+}
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ubah Data Mahasiswa</title>
+  </head>
+  <body>
+    <h1>Ubah data Mahasiswa</h1>
+      <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $mhs["id"];?>">
+        <input type="hidden" name="gambarLama" value="<?= $mhs["gambar"];?>">
+
+          <ul>
+            <li>
+              <label for="nama">NAMA : </label>
+                <input type="text" name="nama" id="nama" required
+                 value="<?= $mhs["nama"]; ?>">
+            </li>
+            <li>
+              <label for="nim">NIM : </label>
+                <input type="text" name="nim" id="nim" required
+                value="<?= $mhs["nim"]; ?>">
+            </li>
+            <li>
+              <label for="email">EMAIL : </label>
+                <input type="text" name="email" id="email"required
+                value="<?= $mhs["email"]; ?>">
+            </li>
+            <li>
+              <label for="jurusan">JURUSAN : </label>
+                <input type="text" name="jurusan" id="jurusan"required
+                value="<?= $mhs["jurusan"]; ?>">
+            </li>
+            <li>
+              <label for="gambar">GAMBAR : </label> <br>
+              <img src="img/<?= $mhs['gambar']; ?>" width="200"><br>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data</button>
+            </li>
+          </ul>
+      </form>
+
+  </body>
+</html>
